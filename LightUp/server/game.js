409b8878f967869030506d95b15bc66b7ddfb133
Lib3Rt9 <phamgiaphuc011709@gmail.com>
2021-11-27 22:34:55 +0700

    update websocket

--- LightUp/server/game.js
+++ LightUp/server/game.js
@@ -1,45 +1,61 @@
-class User {
-    constructor(socket) {
-        this.socket = socket;
-        // assign a random number to User.
-        // Long enough to make duplication chance less.
-        this.id = "1" + Math.floor(Math.random() * 1000000000);
+// store game and logic
+// manage all connected sockets
+// store socket connection objects and create random ID
+function User(socket) {
+    this.socket = socket; 
+    // assign a random number to User.
+    // Long enough to make duplication chance less.
+    this.id = "1" + Math.floor( Math.random() * 1000000000);
+   }
 
-    }
-}
-
-class Room {
-    constructor() {
-        this.users = [];
-    }
-}
-
-Room.prototype.addUser = function(user){
-    this.users.push(user); 
-    var room = this; 
-    // handle user closing
-    user.socket.onclose = function(){
-        console.log('A connection left.');
-        room.removeUser(user);
-    }
-};
-
-Room.prototype.removeUser = function(user) {
-    // loop to find the user
-    for (var i=this.users.length; i >= 0; i--) {
-        if (this.users[i] === user) {
-            this.users.splice(i, 1);
+// store a collection of user instances
+function Room() {
+    this.users = [];
+    
+    // manages the adding and removing of users
+    Room.prototype.addUser = function(user){
+        this.users.push(user);
+        var room = this;
+        
+        this.handleOnUserMessage(user);
+        // handle user closing
+        user.socket.onclose = function(){
+            console.log('A connection left.');
+            room.removeUser(user);
         }
-    }
+
+    };
+
+    Room.prototype.removeUser = function(user) {
+        // loop to find the user
+        for (var i=this.users.length; i >= 0; i--) {
+            if (this.users[i] === user) {
+                this.users.splice(i, 1);
+            }
+        }
+    };
+
+    // sending messages to all the connected users in the room
+    Room.prototype.sendAll = function(message) {
+        for (var i=0, len=this.users.length; i<len; i++) {
+            this.users[i].socket.send(message);
+        }
+    };
+
+    // handles user messages
+    Room.prototype.handleOnUserMessage = function(user) {
+        var room = this;
+        user.socket.on("message", function(message){
+            console.log("Receive message from " + user.id + ": " + message);
+        });
+        socket.on("message", function(message){
+            console.log("Receive message: " + message);
+        });
+    };
 };
 
-Room.prototype.sendAll = function(message) {
-    for (var i=0, len=this.users.length; i<len; i++) {
-        this.users[i].socket.send(message);
-    }
-};
 
-const _User = User;
-export { _User as User };
-const _Room = Room;
-export { _Room as Room };
+
+// define User and Room classes to let other files use them
+module.exports.User = User;
+module.exports.Room = Room;
--- LightUp/server/server.js
+++ LightUp/server/server.js
@@ -1,7 +1,7 @@
 // create a new simple server
 // use 8000 as server port number
 var port = 8000;
-
+// var socket = new WebSocket(url);
 // Server code
 // use 'require' to load a function in a specific module
 var WebSocketServer = require('ws').Server;
@@ -9,9 +9,23 @@ var server = new WebSocketServer({ port: port });
 
 // node.js server is event based
 // listens to the connection event and handles it
+
+// sends the user count to all the connected users
+var User = require('./game').User;
+var Room = require('./game').Room;
+var room1 = new Room();
 server.on('connection', function(socket) {
+    var user = new User(socket);
+    room1.addUser(user);
     console.log("A connection established");
+    var message = "Welcome " + user.id + " joining the party. Total connection: " + room1.users.length;
+    room1.sendAll(message);
+
+    socket.on("message", function(message){
+        console.log("Receive message: " + message);
+    });
 });
+
 
 console.log("WebSocket server is running.");
 console.log("Listening to port " + port + ".");
