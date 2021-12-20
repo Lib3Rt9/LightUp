9a35aab13fc7feaea11ca7200661c0a1ec83b7b2
Lib3Rt9 <phamgiaphuc011709@gmail.com>
2021-12-20 13:25:11 +0700

    fix clear_canvas function

--- LightUp/client/js/websockets.js
+++ LightUp/client/js/websockets.js
@@ -106,7 +106,7 @@ $(function(){
 
                 if (data.gameState === wsGame.GAME_START) {
                     // clear the Canvas.
-                    canvas.width = canvas.width;
+                    clear_canvas();
                     
                     // hide the restart button.
                     $("#restart").hide();
@@ -206,7 +206,7 @@ function sendMessage() {
 $("#restart").hide();
 $("#restart").click(function(){
     
-    canvas.width = canvas.width;
+    clear_canvas();
     $("#chat-history").html("");
     $("#chat-history").append("<li>Restarting Game.</li>");
 
